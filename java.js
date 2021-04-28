b9848086c558c10e7be0e24f14b5ce9c266cf991
Juneus Magsakay <jbm9397@gmail.com>
2021-04-28 06:04:35 -0400

    initial commit

---
+++ java.js
@@ -0,0 +1,17 @@
+var count = 0;
+var countElement = document.querySelector("#count");
+console.log(countElement);
+function add(){
+    count++;
+    countElement.innerText = count + " like (s)";
+    console.log(count);
+}
+
+var count = 0;
+var countElement = document.querySelector("#count2");
+console.log(countElement);
+function add(){
+    count++;
+    countElement.innerText = count + " like (s)";
+    console.log(count);
+}
